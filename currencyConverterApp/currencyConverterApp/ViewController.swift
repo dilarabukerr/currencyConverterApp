230098963e1fdb8d0554b16da0490980786fbdc0
Dilara Büker <72442245+dilarabukerr@users.noreply.github.com>
2024-03-03 00:27:37 +0300

    currencyConverterApp

---
+++ currencyConverterApp/currencyConverterApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  currencyConverterApp
+//
+//  Created by Dilara Büker on 3.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chvLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func getRatesButton(_ sender: Any) {
+        
+    }
+    
+}
+
